3c0e1f96846c30a638b3b9694760d992ef706516
Defless <simon.deflesschouwer@mmibordeaux.com>
2020-02-14 17:58:31 +0100

    Updates

---
+++ precache-manifest.270c5a8030b11bb5d0dce1783a5c125f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2209315a9d7f84ff06b7c7e01c2c622f",
+    "url": "/Mastermind/index.html"
+  },
+  {
+    "revision": "c8111ad9e2298b13779b",
+    "url": "/Mastermind/static/css/main.8f355a2e.chunk.css"
+  },
+  {
+    "revision": "788b826daeb188a498e6",
+    "url": "/Mastermind/static/js/2.af25c92b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Mastermind/static/js/2.af25c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8111ad9e2298b13779b",
+    "url": "/Mastermind/static/js/main.5b78736a.chunk.js"
+  },
+  {
+    "revision": "f29d2b6e924a7bff72f6",
+    "url": "/Mastermind/static/js/runtime-main.0de92b23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Mastermind/static/media/logo.5d5d9eef.svg"
+  }
+]);
